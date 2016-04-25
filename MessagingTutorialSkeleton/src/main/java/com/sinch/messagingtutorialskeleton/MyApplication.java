e9f052f8c5d3422ea254e5bed2e6c07068ac1739
Sakurami-m <blacky.rose.m@gmail.com>
2016-04-25 07:21:45 +0300

    Update MyApplication.java
    Code cleaning

--- MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/MyApplication.java
+++ MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/MyApplication.java
@@ -1,7 +1,7 @@
 package com.sinch.messagingtutorialskeleton;
 
 /**
- * Created by Haya on 10/27/2015.
+ * Created by Haya on 1/4/2016.
  */
 import android.app.Application;
 import android.util.Log;
@@ -38,69 +38,14 @@ public class MyApplication extends Application {
             //add the data in the list to the array
             for (ParseObject comment : textMSGList) {
                 array[g]=comment.get("FilteredMsg").toString();
-                // Log.i("testing ",array[g]);
                 g++;
             }
-            //  Apriori.main(array);
-
         }
         catch (Exception e)
         {
             Toast.makeText(this, "تأكد من اتصالك بالانترنت", Toast.LENGTH_LONG).show();
 
         }
-
- /*       try {
-         System.out.println("start");
-            ParseQuery<ParseObject> query1 = ParseQuery.getQuery("ParseMessage");
-            System.out.println("start2");
-
-        List<ParseObject> Objectslist = null;
-            System.out.println("start4");
-
-            Objectslist = query1.find();
-
-        for (int index = 40; index < 53; ++index) {
-            String str = Objectslist.get(index).getString("messageText");
-            System.out.println("start5");
-
-            PrepareData.AddaConversation(str);
-        }
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }*/
-
-
-/*        //array to covert list to array
-        String [] array = new String[textMSGList.size()];
-        int g=0;
-        //add the data in the list to the array
-        for (ParseObject comment : textMSGList) {
-            array[g]=comment.get("FilteredMsg").toString();
-         Log.i("testing ",array[g]);
-            g++;
-        }
-
-
-String [] test ={"1 2 3 4",
-        "1 5 6 7",
-        "1 8 9 10",
-        "1 2 3 4",
-        "1 2 3 4",
-        "10 11 12 13"};
-        try {
-            Log.i("before ","before apriori");
-            Apriori.main(array);
-            Log.i("after ", "after apriori");
-
-        } catch (Exception e) {
-            Log.i("try exception", "try exception");
-
-            e.printStackTrace();
-        }
-
-*/
 
     }
 }
